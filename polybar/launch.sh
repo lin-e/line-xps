e3e76554b9d2118618ec8773983cd26a6f723732
me@eugenel.in <me@eugenel.in>
2020-06-14 02:38:16 +0100

    Modified polybar to support multiple screens

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 60; done
-MONITOR=eDP-1 polybar top &
+
+monitors=$(xrandr -q | grep " connected" | cut -d ' ' -f1)
+IFS='\n'
+
+echo $monitors | while IFS= read -r line; do
+  MONITOR=$line polybar top &
+done
